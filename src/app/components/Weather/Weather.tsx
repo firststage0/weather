e43346f1bca83674abf6b40bd062692abcba93e7
Vladimir Tkachev <90560402+firststage0@users.noreply.github.com>
2024-04-05 23:54:41 +0300

    T-1 Сделать получение данных о погоде по городу (#1)
    * добавлен автокомплит
    
    * добавлена возможность получать погоду из api запроса
    
    * добавлена возможность получать температуру из api запроса
    
    * hot fix
    
    * T-1 Сделать получение данных о погоде по городу
    
    * T-1 Сделать получение данных о погоде по городу и добавить загрузку
    
    * T-1 Сделать получение данных о погоде по городу и добавить загрузку
    
    ---------
    
    Co-authored-by: coca8732 <nub465@gmail.com>

--- src/app/components/Weather/Weather.tsx
+++ src/app/components/Weather/Weather.tsx
@@ -1,14 +1,49 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
+import { Autocomplete, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 
-export const Weather = () =>{
-	const [data, setData] = useState<any>([]);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
+const apiKey = "57f7df1e3063971e738d4e9c5af1bb15";
+const listOfCities = ["Москва", "Воронеж", "Самара", "Санкт-петербург"];
+export const Weather = () => {
+  const [cityName, setCityName] = useState<string | null>(listOfCities[0]);
+  const [weatherData, setWeatherData] = useState<any>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-	useEffect(() => {
-		
-	}, []);
+  useEffect(() => {
+    setIsLoading(true);
+  	const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    fetch(apiURL)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Response isn`t ok");
+        }
+        return res.json();
+      })
+      .then(data => {
+        setWeatherData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      })
+  }, [cityName]);
 
-	return <div>Po</div>
-}
+  return (
+    <div style={{ padding: "20px" }}>
+      <Autocomplete
+        value={cityName}
+        onChange={(event: any, newValue: string | null) => {
+          setCityName(newValue);
+        }}
+        disablePortal
+        id="combo-box-demo"
+        options={listOfCities}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Город" />}
+      />
+
+      { isLoading ? <div>Загрузка...</div> : <div >{`Температура в городе ${cityName} составляет ${weatherData?.main.temp} `}</div>}
+    </div>
+  );
+};
