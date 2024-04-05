282aa19f5515b7b07f937a7ffe0247fddc054836
firststage <red_tk@mail.ru>
2024-04-05 18:03:44 +0300

    added weather project

---
+++ my-app/src/app/components/Weather/Weather.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+
+export const Weather = () =>{
+	const [data, setData] = useState<any>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		
+	}, []);
+
+	return <div>Po</div>
+}
---
+++ my-app/src/pages/Main/Main.tsx
@@ -0,0 +1,7 @@
+import { Weather } from "@/app/components/Weather/Weather";
+
+const Main = () => {
+	return <Weather />
+}
+
+export default Main;
